81f97705e9e6d8383827522a8ad45bcaba800392
theArctesian <stephen.d.okita@gmail.com>
2023-03-02 16:56:05 +0800

    hole

--- DB/GoodReads/cleanGoodReads.py
+++ DB/GoodReads/cleanGoodReads.py
@@ -1,4 +1,32 @@
 import pandas as pd
 
-df = pd.read_csv("./good_reaads_library_export.csv")
-print(df)
+df = pd.read_csv("./goodreads_library_export.csv")
+#print(df.to_string()) 
+df = df.drop(columns=["Book Id",
+                      'Author l-f',
+                      'Additional Authors', 
+                      'ISBN',
+                      "ISBN13",
+                      "My Rating",
+                      "Average Rating",
+                      "Publisher",
+                      "Binding",
+                      "Number of Pages",
+                      "Year Published",
+                      "Date Read",
+                      "Date Added",
+                      "Bookshelves",
+                      "Bookshelves with positions",
+                      "Exclusive Shelf",
+                      "My Review",
+                      "Spoiler",
+                      "Private Notes",
+                      "Owned Copies"
+                    ])
+print(df.columns.values)
+print(df.info)
+
+import csv
+
+with open('data.csv', 'w', newline='') as f:
+    df.to_csv('data.csv', index=False, header=True)
--- DB/LetterBox/cleanLetterBox.py
+++ DB/LetterBox/cleanLetterBox.py
@@ -1,18 +1,33 @@
 import json
-import coalas
+import pandas as pd
+import numpy as np
+
 class Schema:
     def __init__(self, Name, Author, Image, DatePublished, DateRead, Watched):
         self.Name = Name
         self.Author = Author
-        self.Image = Image
         self.DatePublished = DatePublished
-        self.DateRead = DateRead
         self.Watched = watched
-
-# I am locking at this class and relising I don't like OOP in this language
-# Even TS is better, I have no type saftey on init
 
 
 if __name__ == "__main__":
-    data = coalas.csvReader(watcher.csv)
-    print(data)
+    # Read
+    wdf = pd.read_csv("./watched.csv")
+    ldf = pd.read_csv("./watchlist.csv")
+
+    # Parse
+    wdf = wdf.drop(columns=["Date", "Letterboxd URI"])
+    ldf = ldf.drop(columns=["Date", "Letterboxd URI"])
+
+    # Add watcher var
+    wdf.insert(2, "Watched", [1] * (len(wdf)), True)
+    ldf.insert(2, "Watched", [0] * (len(ldf)), True)
+    wdf.insert(1, "Watched", [""] * (len(wdf)), True)
+    ldf.insert(1, "Watched", [""] * (len(ldf)), True)
+
+    # Add empty Author Line (#TODO add director names)
+
+    print(type(wdf.columns.values))
+    print(wdf.shape[0])
+
+    wdf.to_json('data.json', orient="values")
